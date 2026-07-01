cd2c32cbfd1d4c73e148e6759823e0565ed25223
SiyonaRahane <rahanesiyona@gmail.com>
2026-07-01 21:14:06 +0530

    add complete files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Welcomes to AI Project</h1>
+      <p>Welcome to your Vite + React project!</p>
+    </>
+  )
+}
+
+export default App
