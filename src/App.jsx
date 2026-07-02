26551b36d81644d2cb09d58f46475aeb742242f0
SiyonaRahane <rahanesiyona@gmail.com>
2026-07-02 13:09:13 +0530

    change subheading

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Welcomes to AI Project</h1>
-      <p>Welcome to your Vite + React project!</p>
+      <p>I want to create the LLM Model by using open AI</p>
     </>
   )
 }
-
 export default App
